ed20f42ca3bfbd5f39db4566a5e5d84c5b8cef2d
= <cadastro@diegogaspar.dev.br>
2023-03-07 11:42:32 -0300

    Fix: type_user

--- users/views.py
+++ users/views.py
@@ -6,9 +6,9 @@ from .permissions import IsUserPermission
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 import ipdb
+from rest_framework.views import Response, status
 
 
-# Create your views here.
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -24,3 +24,20 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsUserPermission]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        if "type_user" in self.request.data:
+            if self.request.data["type_user"] == "admin":
+                return Response({"message": "Permission"}, status=status.HTTP_401_UNAUTHORIZED)
+
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
