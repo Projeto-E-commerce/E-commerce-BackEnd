127025824f2fff345a74c12d02cdddef1f7d2267
naoeoneto <abdpneto@gmail.com>
2023-03-08 16:34:40 -0300

    feat: update cartproduct logic

--- carts/models.py
+++ carts/models.py
@@ -5,12 +5,6 @@ from products.models import Product
 
 # Create your models here.
 class Cart(models.Model):
-    total_price = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        null=True,
-    )
-
     cart_products = models.ManyToManyField(
         Product, through="carts.CartProduct", related_name="shopping_cart"
     )
@@ -28,3 +22,8 @@ class CartProduct(models.Model):
     )
 
     product_count = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+    )
