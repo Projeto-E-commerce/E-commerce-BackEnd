6cac75b8002d1948635096e9bec266df6b77f9da
Victor Novakoski <Novakoski2021@gmail.com>
2023-03-10 12:05:26 -0300

    feat: add cors and doc

--- _ecommerce/urls.py
+++ _ecommerce/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,4 +27,15 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("carts.urls")),
     path("api/", include("orders.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    # path(
+    #     "api/docs/swagger-ui/",
+    #     SpectacularSwaggerView.as_view(url_name="schema"),
+    #     name="swagger-ui",
+    # ),
 ]
